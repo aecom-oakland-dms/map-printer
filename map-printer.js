4a370b7da5f58700780fa0c394817565cf157e65
Ben Kane <ben.kane@aecom.com>
2015-12-25 19:28:36 -0800

    modified code in evaluatePage to actually wait for layers to add and layerconfig to load.  cleaned up page methods and organized code.

--- map-printer.js
+++ map-printer.js
@@ -23,12 +23,13 @@ Queue.prototype.list = [];
 // cleanup the temporary files even when an uncaught exception occurs.
 // tmp.setGracefulCleanup();
 
-function setPageSize(page, options){
+function setPageSize(options){
     options = options || {};
 // function setPageSize(page, pageSize, pageOrientation, ph){
     // let pageSize = "A4"
     // let pageOrientation = "landscape",
     let temp
+    , page = options.page
     , format = options.format
     , orientation = options.orientation
     // , dpi = 300
@@ -142,7 +143,7 @@ function setPageSize(page, options){
 // let filetype = '.pdf';
 // let filetype = '.png';
 // let filetype = '.jpg';
-function openPageAtUrl(page, url, options, callback){
+function openPage(options){
     console.log('opening page at:', url);
     
     let orientation = options.orientation || 'Landscape'
@@ -150,147 +151,152 @@ function openPageAtUrl(page, url, options, callback){
     , format = options.format = `${options.format || 'Letter'}`
     ;
     // let orientation = 'Portrait';
-    options.pageSize = setPageSize(page, options);
+    options.pageSize = setPageSize(options);
 
     options.bodyheight = options.height;
     options.bodywidth = options.width;
     
-    page.open( url, function(status){
+    options.page.open( options.url, function(status){
          if (status !== 'success') {
             console.log('Unable to access network', status);
             setTimeout(function(){
-                page.close
+                options.page.close();
+                delete options.page
                 // ph.exit()
             }, 500)
             // // ph.exit(1);  // thow error if you want to
-            if(callback && callback instanceof Function)
-                return callback({error: ['phantom unable to open webpage at', url].join(' '), status: status})
+            if(options.callback && options.callback instanceof Function)
+                return options.callback({error: 'phantom unable to open webpage at ' + options.url, status: status})
         } else 
-            onPageOpen(page, options, url, callback)
+            onPageOpen(options)
     });
 }
 
-function onPageOpen(page, options, url, callback){
-    page.evaluate( 
-        function(options){ 
-            'use strict';
-            /* run javascript on webpage */
-            window.phantom = true;
-            options = options || {};
-
-            console.log('running javascript on page via phantom', 'window.phantom?', window.phantom);
-
-            // add phantom class to styles in phantom-mods.css are applied
-            $('html')
-                .addClass('phantom')
-                // .addClass(options.orientation || '')
-            ;
-            
-            var size = options.pageSize || {}
-            , filetype = options.filetype || ''
-            ;
-            
-            // add the pageinfo
-            // $('footer').append(
-            //     "<table width='100%'>" + 
-            //       "<tr><td class='pull-left'><small> " + (pageOptions.accessnote || '') + "</small></td>" +
-            //       "<td>&nbsp;</td> <td>&nbsp;</td>" +
-            //       "<td colspan='2' style='text-align: right;'><small>" + pageOptions.pageNum + " / " + pageOptions.numPages + "</small></td></tr>" +
-            //       "</table>"
-            // );
-
-            // adjustments for pdf output
-            // if(filetype==='.pdf'){
-            var width = options.bodywidth || size.width || 800
-            , height = options.bodyheight || size.height || 900
-            ;
-            
-            // console.log('size:', JSON.stringify(size));
-            // console.log('height:', height);
-            // console.log('width:', width);
-
-            [window, 'html, body'].forEach(function(name){
-                var whb = $(name).css({
-                    overflow: 'hidden'
-                    , width: width
-                    , 'max-width' : width
-                    , 'height': height
-                    , 'max-height': height
-                })
-
-                // console.log('whb:', name, JSON.stringify({size: {height: whb.height(), width: whb.width()}}) )
-            });
-            // }
-             
-            // remove all href's because they aren't necessary and they render sometimes in the PDF output
-            $('a').attr('href', null);
-
-            $('.markerLabel').removeClass('trans')
-            //     .css('font-size', '+=15px');
-
-            // hide legend items for layer's that aren't visible
-            // $('.legendGroup .layer-toggle:not(:checked)').parent().hide()
-            // $('.layer-toggle:not(:checked)').closest('.legendGroup').hide()
-            // $('.legendGroup[class*=" disabled-"]').hide()
-             
-            var printmessage = 'PAGE READY FOR PRINTING'
-            , timer
-            ;
-            function triggerPrint(){
-                clearTimeout(timer);
-                $(app).trigger('print');
-                console.log(printmessage)
-            }
-            
-            // send it after 30 seconds if not sent on layerconfigs:loaded
-            timer = setTimeout(triggerPrint, 30 * 1000);
-            // var timer = setTimeout(function(){
-            //     console.log('timed out waiting for layerconfigs to load...sending printmessage now anyway')
-            //     $(app).trigger('print');
-            //     console.log(printmessage)
-            // }, 30 * 1000);
-
-            // or when the map is finished loading layerconfigs
-            app.map.on('layerconfigs:loaded', triggerPrint)
-                // function(){
-            //     // hide/remove non-active layers from the print output
-            //     $(app).trigger('print');
-            //     clearTimeout(timer);
-            //     console.log(printmessage)
-            // });
-
-            console.log('DONE RUNNING JAVASCRIPT ON PHANTOMJS PAGE');
-            return {
-                title: document.title
-            }
-        }
+function onPageOpen(options){
+    options.page.evaluate( 
+        evaluatePage
         // callback after page.evaluate finished
-        , function onEvaluated(args){
-            // console.log('onEvaluated args:', arguments);
-            args = args || { title: 'test-title'};
-            
-            let title = args.title
-            , name = createFileName(title,url)
-            ;
-            
-            console.log('Creating', name);
-            options.resourceQueue.then(
-                function(){
-                    createPDF(page, {filetype: options.filetype, outfile:name }, callback);
-                }    
-            )
-        } 
+        , onPageEvaluated.bind(options)
         // pass arguments into page evaluate
         , options
     )
 }
 
+/// this function cannot access anything outside of itself
+/// or the options object passed in
+/// it is run in the "browser" context, not the node context
+function evaluatePage(options){ 
+    'use strict';
+    /* run javascript on webpage */
+    window.phantom = true;
+    options = options || {};
+
+    // if(typeof app == 'undefined'){
+    //     console.log('`app` is undefined, creating it as emtpy object');
+    //     var app = {};
+    // }
+
+    console.log('running javascript on page via phantom', 'window.phantom?', window.phantom);
+
+    // add phantom class to styles in phantom-mods.css are applied
+    $('html, body')
+        .addClass('phantom')
+        // .addClass(options.orientation || '')
+    ;
+    
+    var size = options.pageSize || {}
+    , filetype = options.filetype || ''
+    ;
+    
+    // add the pageinfo
+    // $('footer').append(
+    //     "<table width='100%'>" + 
+    //       "<tr><td class='pull-left'><small> " + (pageOptions.accessnote || '') + "</small></td>" +
+    //       "<td>&nbsp;</td> <td>&nbsp;</td>" +
+    //       "<td colspan='2' style='text-align: right;'><small>" + pageOptions.pageNum + " / " + pageOptions.numPages + "</small></td></tr>" +
+    //       "</table>"
+    // );
+
+    // adjustments for pdf output
+    // if(filetype==='.pdf'){
+    var width = options.bodywidth || size.width || 800
+    , height = options.bodyheight || size.height || 900
+    ;
+    
+    // console.log('size:', JSON.stringify(size));
+    // console.log('height:', height);
+    // console.log('width:', width);
+
+    [window, 'html, body'].forEach(function(name){
+        var whb = $(name).css({
+            overflow: 'hidden'
+            , width: width
+            , 'max-width' : width
+            , 'height': height
+            , 'max-height': height
+        })
+
+        // console.log('whb:', name, JSON.stringify({size: {height: whb.height(), width: whb.width()}}) )
+    });
+    // }
+     
+    // remove all href's because they aren't necessary and they render sometimes in the PDF output
+    $('a').attr('href', null);
+
+    $('.markerLabel').removeClass('trans')
+    //     .css('font-size', '+=15px');
+     
+    var printmessage = 'PAGE READY FOR PRINTING'
+    , timer
+    ;
+
+    function triggerPrint(msg){
+        clearTimeout(timer);
+        $(window).trigger('print:start');
+        console.log('+++++', msg, '+++++')
+        console.log(printmessage);
+    }
+    
+    // send it after 30 seconds if not sent on layerconfigs:loaded
+    timer = setTimeout(triggerPrint, 30 * 1000);
+
+    // or when the map is finished loading layerconfigs
+    if(window.layerconfigs_loading){
+        var configs = $.extend({}, window.layerconfigs_loading)
+        console.log('need to wait for layerconfigs', window.layerconfigs_loading);
+        $(window).on('layerconfigs:loaded', function(){
+            console.log('triggering print from layerconfigs:loaded');
+            triggerPrint('waited for ' + JSON.stringify(configs));
+        })
+    }else
+        triggerPrint("didn't wait for any layerconfigs")
+
+    console.log('DONE RUNNING JAVASCRIPT ON PHANTOMJS PAGE');
+    return {
+        title: document.title
+    }
+}
+
+function onPageEvaluated(args){
+    // console.log('onEvaluated args:', arguments);
+    args = args || { title: 'test-title'};
+    
+    let title = args.title
+    , name = createFileName(title, this.url);
+    ;
+    
+    this.resourceQueue.then(()=>{
+        console.log('Creating', name);
+        createPDF(this.page, {filetype: this.filetype, outfile:name }, this.callback);
+    })
+} 
+
 function createPDF(page, options, callback){
     // hacky stupid way to wait for it to load
-    setTimeout(function(){
+    // setTimeout(function(){
         console.log('creating temp file with prefix:', options.outfile, 'postfix:', options.filetype);
         tmp.file({ prefix: options.outfile, postfix: options.filetype, keep: true }, function _tempFileCreated(err, filepath, fd, cleanupCallback) {
-          if (err) return console.error(err)//throw err;
+          if (err) return console.error('error creating temp file:', err)//throw err;
           
           page.render(filepath, {quality: options.quality || '100'} );
           setTimeout(function(){
@@ -306,9 +312,9 @@ function createPDF(page, options, callback){
               setTimeout(t=>page.close(), 3000);
 
               // ph.exit();
-          }, 500)
+          }, 1000)
         });
-    }, 1500)
+    // }, 1500)
 }
 
 function createFileName(title, url){
@@ -348,12 +354,16 @@ function loadPhantom(cb){
 function makeMap(url, options, callback){
     console.log('url passed to map-printer.makeMap:', url);
     // try{
+    options.url = url;
+    options.callback = callback;
       
     if(!phantomPH)
         return loadPhantom( ()=>makeMap.apply(null, arguments) )
       // phantom.create(function (ph) {
 
     phantomPH.createPage(function (page) {
+        options.page = page;
+
         let requests = new Queue()
         , timelimit = 5000
         , resourceQueue = new Promise((resolve, reject)=>{
@@ -415,10 +425,10 @@ function makeMap(url, options, callback){
                 console.log('\n** requests remaining at time when page ready from printing:', requests.list.join('\n\t * '), '\n')
             }
 
-            console.log('webpage console message:', msg, arguments);
+            console.log('webpage console message:', msg);
         });
 
-        openPageAtUrl(page, url, options, callback)
+        openPage(options)
     });
 }
 
